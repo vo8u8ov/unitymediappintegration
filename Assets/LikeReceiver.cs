ed64bc23af70002bf705497811f5f738045a4212
Mariko.Sakai <>
2025-05-03 16:34:22 +0900

    パーティクルエフェクトオンオフ

--- Assets/LikeReceiver.cs
+++ Assets/LikeReceiver.cs
@@ -5,16 +5,35 @@ using extOSC;
 
 public class LikeReceiver : MonoBehaviour
 {
+    public ParticleSystem likeParticles;  // ← Unity上でInspectorにドラッグ
+    private OSCReceiver receiver;
     // Start is called before the first frame update
     void Start()
     {
-        var receiver = gameObject.AddComponent<OSCReceiver>();
+        receiver = gameObject.AddComponent<OSCReceiver>();
         receiver.LocalPort = 9000;
         receiver.Bind("/like", OnLike);
     }
 
     void OnLike(OSCMessage message)
     {
-        Debug.Log("LIKE! を受信しました！");
+        Debug.Log(message.ToString());  // ← デバッグ用
+        int likeState = message.Values[0].IntValue;
+
+        if (likeState == 1)
+        {
+            if (!likeParticles.isPlaying)
+            {
+                likeParticles.Clear();
+                likeParticles.Play();
+            }
+        }
+        else
+        {
+            if (likeParticles.isPlaying)
+            {
+                likeParticles.Stop();
+            }
+        }
     }
 }
